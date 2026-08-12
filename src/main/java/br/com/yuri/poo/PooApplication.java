659f874f83b4b098ad7031c846cbc9c477842799
YuriGobatto <yuri.signacio@hotmail.com>
2026-08-12 17:05:24 -0400

    Aula 11/08 - Iniciando Spring

---
+++ src/main/java/br/com/yuri/poo/PooApplication.java
@@ -0,0 +1,13 @@
+package br.com.yuri.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooApplication.class, args);
+	}
+
+}
